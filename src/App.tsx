53a39d04b8101a37a4ea8f0485a57956ff76f0b0
mizanur0711 <mizanur0711@gmail.com>
2023-06-01 18:31:21 +0600

    task-2-final

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlert] = useState(false);
   return (
     <div>
+      {alertVisible && <Alert onClose={() => setAlert(false)}>Alert</Alert>}
       <Button
         children="primary"
         color="secondary"
-        Onclick={() => console.log("clicked")}
+        Onclick={() => setAlert(true)}
       />
     </div>
   );
