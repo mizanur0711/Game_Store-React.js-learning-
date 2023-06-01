50d690f7036c60e635a588c69b765fe2985286f1
mizanur0711 <mizanur0711@gmail.com>
2023-06-01 18:21:55 +0600

    function passing via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import ListGroup from "./components/ListGroup";
+
 function App() {
   let items = ["Dhaka", "Chattrogram", "Rangpur", "Khulna"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
