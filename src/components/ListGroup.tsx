1501b4a2bda5475f943480b92a1b1d35dc57f3c7
mizanur0711 <mizanur0711@gmail.com>
2023-06-01 18:10:25 +0600

    set selected using use state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,24 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 function ListGroup() {
   let items = ["Rice", "Water", "salad", "curry"];
-
-  //Event Handler
-  const HandleClick = (event: MouseEvent) => console.log(event);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={HandleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
