8de8a6d6e7511f0a7b0221635cd927ce22e9f37a
mizanur0711 <mizanur0711@gmail.com>
2023-06-01 18:08:40 +0600

    handling event by arrow func

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,16 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["Rice", "Water", "salad", "curry"];
+
+  //Event Handler
+  const HandleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(item, index)}
-          >
+          <li className="list-group-item" key={item} onClick={HandleClick}>
             {item}
           </li>
         ))}
