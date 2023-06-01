4f3a21fe157fc05cc25b56682bd342b813d84261
mizanur0711 <mizanur0711@gmail.com>
2023-06-01 18:05:52 +0600

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,16 @@
 function ListGroup() {
-  const items = ["Rice", "Water", "salad", "curry"];
+  let items = ["Rice", "Water", "salad", "curry"];
+  items = [];
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item Found</p> : null}
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
