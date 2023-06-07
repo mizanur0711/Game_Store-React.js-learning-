056c85b1698fc21b9c7f5d868aaa4a6ad552d827
mizanur0711 <mizanur0711@gmail.com>
2023-06-07 18:29:18 +0600

    form creation,validity with zod,update array,reset form

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { ExpenseFilter } from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -18,7 +17,11 @@ function App() {
   return (
     <div>
       <div className="mb-5">
-        <ExpenseForm></ExpenseForm>
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        ></ExpenseForm>
       </div>
       <div className="mb-3">
         <ExpenseFilter
